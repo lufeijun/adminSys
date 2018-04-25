de2a14a54a767075c61614aa611519355ae64d18
lufeijun1234 <lufeijun_1234@126.com>
2018-04-25 15:12:45 +0800

    创建数据库迁移migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,11 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('image');
+            $table->string('role');
+            $table->tinyInteger('enable');
             $table->rememberToken();
             $table->timestamps();
         });
--- readme.md
+++ readme.md
@@ -11,7 +11,7 @@
 - 使用git克隆整个项目，并将bootstrap/cache/* ，storage/* 权限设置为777；
 - 执行php composer install，处理了依赖关系，下载相关安装包；
 - 在public目录下建立sys_images，并将权限赋为777。
-- 创建对应数据库。
+- 创建对应数据库。php artisan migrate
 
 ## 截图展示
 <img src="https://github.com/lufeijun/adminSys/blob/master/public/img/2018/home.png">
